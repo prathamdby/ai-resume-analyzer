a4eb8778818057ffc42e7d1f81359d5602ae8f6d
Pratham Dubey <134331217+prathamdby@users.noreply.github.com>
2025-09-30 17:07:06 +0530

    Add input field styles and hide Puter splash screen

--- app/root.tsx
+++ app/root.tsx
@@ -41,7 +41,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        <script src="https://js.puter.com/v2/"></script>
+        <script src="https://js.puter.com/v2/" data-splash="false"></script>
 
         {children}
         <ScrollRestoration />
